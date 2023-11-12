05381671a1f00ddc2212b38c682780d0fa949c6a
Your Name <you@example.com>
2023-11-12 14:21:29 +0530

    commit in new branch

--- branch.js
+++ branch.js
@@ -1,1 +1,3 @@
 console.log("branch1")
+console.log("branch1")
+console.log("branch1")
---
+++ vishvas.js
@@ -0,0 +1,5 @@
+console.log("this is vishvas kumar")
+console.log("this is vishvas kumar")
+console.log("this is vishvas kumar")
+console.log("this is vishvas kumar")
+console.log("this is vishvas kumar")
